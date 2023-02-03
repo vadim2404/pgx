e1864e354e9f957e9a159c0a3c4e16cba6c7db11
Vadim Kharitonov <vadim@neon.tech>
2023-02-03 13:43:20 +0100

    Neon ABI

--- pgx/src/lib.rs
+++ pgx/src/lib.rs
@@ -221,7 +221,7 @@ macro_rules! pg_magic_func {
                 float8byval: cfg!(target_pointer_width = "64") as i32,
                 abi_extra: {
                     // array::from_fn isn't const yet, boohoo, so const-copy a bstr
-                    let magic = b"PostgreSQL";
+                    let magic = b"Neon Postgres";
                     let mut abi = [0 as ::pgx::ffi::c_char; 32];
                     let mut i = 0;
                     while i < magic.len() {
